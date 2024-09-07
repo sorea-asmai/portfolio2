33765853c2286221aa037bf67ffb1f9ad59f1953
Sorea A <56051252+sorea19@users.noreply.github.com>
2024-09-07 09:51:07 -0700

    Sync

--- app/routes/home/home.jsx
+++ app/routes/home/home.jsx
@@ -41,7 +41,7 @@ export const links = () => {
 
 export const meta = () => {
   return baseMeta({
-    title: 'Designer + Developer',
+    title: 'Aspiring Developer',
     description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
   });
 };
