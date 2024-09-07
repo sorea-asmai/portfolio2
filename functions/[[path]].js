529f50a0a29d4e88b24b121416100116f7a27738
Sorea A <56051252+sorea19@users.noreply.github.com>
2024-09-06 20:39:18 -0700

    Sync

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -2,7 +2,4 @@ import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 
 import * as build from '../build/server';
 
-import pkg from 'react-dom/server';
-const { renderToReadableStream } = pkg;
-
 export const onRequest = createPagesFunctionHandler({ build });
